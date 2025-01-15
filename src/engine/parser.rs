09d554a56ae36d38c26393d0d9f577bb0facb062
hayatroid <hayatroid@gmail.com>
2025-01-16 07:29:56 +0900

    feat: define enum ParseError

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -1,3 +1,5 @@
+use thiserror::Error;
+
 #[derive(Debug)]
 pub enum AST {
     Char(char),
@@ -6,4 +8,18 @@ pub enum AST {
     Question(Box<AST>),
     Or(Box<AST>, Box<AST>),
     Seq(Vec<AST>),
+}
+
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("ParseError: invalid escape: pos = {0}, char = '{1}'")]
+    InvalidEscape(usize, char),
+    #[error("ParseError: invalid right parenthesis: pos = {0}")]
+    InvalidRightParen(usize),
+    #[error("ParseError: no previous expression: pos = {0}")]
+    NoPrev(usize),
+    #[error("ParseError: no right parenthesis")]
+    NoRightParen,
+    #[error("ParseError: empty expression")]
+    Empty,
 }
